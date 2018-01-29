6856b3ceb2618b62be0ff7f8d6a2896675506ed1
sunche <sunche@umich.edu>
2018-01-29 14:13:23 -0500

    Added question validation and quit() function

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,8 @@
 
 
-ques = input("What is your question?")
+while(True):
+    ques = input("What is your question or enter exit? ")
+    if ques == "exit":
+        quit()
+    elif ques[-1] != "?":
+        print("I’m sorry, I can only answer questions")
